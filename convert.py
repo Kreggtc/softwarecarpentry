019046cb52fdc4c5b10e4406ebc90ab5cd98991e
Aria Novianto <ariapn@gmail.com>
2018-01-19 10:47:08 -0600

    add convert

--- convert.py
+++ convert.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-def convertDollarsToCents ( dollars ):
-    return dollars * 100
-=======
-
 def convertDollarsToCents( dollars ):
-	return dollars * 100
->>>>>>> 44ca18abcf9a2be6629f17939ebb84e646fede35
+    return dollars * 100
